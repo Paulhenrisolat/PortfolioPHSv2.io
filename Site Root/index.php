6af8f7e84731abc15b3df82c9290f69ad187ed06
Paul-Henri SOLAT <paul-henri.solat@lyc-bonaparte.fr>
2021-08-21 17:30:20 +0200

    2eme test

---
+++ Site Root/index.php
@@ -0,0 +1,19 @@
+<?php
+    echo "hi";
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
+        <meta name="description" content="Portfolio Paul-Henri Solat - BTS SIO SLAM">
+        <meta name="author" content="Paul-Henri Solat">
+        <title>Paul-Henri Solat Portfolio</title>
+    </head>
+    
+    <body>
+        <p>Hi !</p>
+            
+    </body>
+</html>
+
